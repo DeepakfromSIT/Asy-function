4f354e3f2d3ee66700247338601c61d8f319960c
DeepakfromSIT <124372302+DeepakfromSIT@users.noreply.github.com>
2024-01-04 19:44:21 +0530

    Update app28.js

--- app28.js
+++ app28.js
@@ -7,10 +7,10 @@ async function getFacts() {
     console.log(data.fact);
 
     let res2 = await fetch(url);
-    let data2 = await res.json();
+    let data2 = await res2.json();
     console.log(data2.fact);
   } catch (e) {
     console.log("error-", e);
   }
-  console.log(getFacts());
+
 }
